e587438319d963e6f1f5efac42dffccab30021f4
Bulmaroz <bzavala@rdms.mx>
2025-07-02 11:21:23 -0600

    Simplificar Program.cs para usar solo Swagger

--- Program.cs
+++ Program.cs
@@ -3,14 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configurar la base de datos
+// Conexion la base de datos
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Agregar controladores
 builder.Services.AddControllers();
 
-// Configurar Swagger
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
